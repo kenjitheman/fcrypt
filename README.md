a917e56d06c1164c387b944ddf78623f7e9cd8a1
kenjitheman <kenjitheman@proton.me>
2023-08-26 17:02:57 +0300

    feat: encryption/decryption logic implemented

--- README.md
+++ README.md
@@ -29,7 +29,37 @@ go get github.com/kenjitheman/fcrypt
 ## usage
 
 ```
+package main
 
+import (
+	"fmt"
+
+	"github.com/kenjitheman/fcrypt"
+)
+
+func main() {
+	key := []byte("examplekeyexampl") // 16, 24, or 32 bytes for AES-128, AES-192, AES-256
+
+	inputFile := "./man.txt"
+	encryptedFile := "./encrypted.enc"
+	decryptedFile := "./decrypted.txt"
+
+	err := fcrypt.EncryptFile(key, inputFile, encryptedFile)
+	if err != nil {
+		fmt.Println("[ERROR] encryption error:", err)
+		return
+	}
+
+	fmt.Println("[SUCCESS] file encrypted successfully")
+
+	err = fcrypt.DecryptFile(key, encryptedFile, decryptedFile)
+	if err != nil {
+		fmt.Println("[ERROR] decryption error:", err)
+		return
+	}
+
+	fmt.Println("[SUCCESS] file decrypted successfully")
+}
 ```
 
 ## contributing
--- core/core.go
+++ core/core.go
@@ -1,1 +1,72 @@
 package core
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+	"os"
+)
+
+func EncryptFile(key []byte, inputFile, outputFile string) error {
+	plaintext, err := os.ReadFile(inputFile)
+	if err != nil {
+		return err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	iv := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return err
+	}
+
+	stream := cipher.NewCTR(block, iv)
+	ciphertext := make([]byte, len(plaintext))
+	stream.XORKeyStream(ciphertext, plaintext)
+
+	output, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+
+	_, err = output.Write(iv)
+	if err != nil {
+		return err
+	}
+
+	_, err = output.Write(ciphertext)
+	return err
+}
+
+func DecryptFile(key []byte, inputFile, outputFile string) error {
+	ciphertext, err := os.ReadFile(inputFile)
+	if err != nil {
+		return err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCTR(block, iv)
+	plaintext := make([]byte, len(ciphertext))
+	stream.XORKeyStream(plaintext, ciphertext)
+
+	output, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+
+	_, err = output.Write(plaintext)
+	return err
+}
